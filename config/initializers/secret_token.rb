26ccba6f837313994dd799cd7a627c1bef1d0445
Levan <levanlifan@gmail.com>
2014-03-19 14:29:27 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shui::Application.config.secret_key_base = '69d79ecb817974a4e69525ac2c9853d2cd44f3a8c8b3af76e8058ef012439ba4b950ea0e82b7e1c0b1c227a93520a162e6e9d23ff7116d2ff4b2b4c7bd357ec3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shui::Application.config.session_store :cookie_store, key: '_shui_session'
